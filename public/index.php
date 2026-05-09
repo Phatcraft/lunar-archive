54be104ea6fae7b289f38642d27b80866aae1f65
Phatcraft <phathong0727@gmail.com>
2026-05-09 14:55:18 +0700

    source code

---
+++ public/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lunar Archive</title>
+
+    <!-- CDNs -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    
+    <nav class="navbar flex-column bg-dark text-white p-0 pt-3 justify-content-center sticky-top" data-bs-theme="dark">
+        <h5 class="m-1">Lunar Archive</h5>
+
+        <div class="nav nav-underline">
+            <a href="/" class="nav-link text-white active">Trang chủ</a>
+            <a href="" class="nav-link text-white">Tài khoản</a>
+        </div>
+    </nav>
+
+    <main class="m-3">
+        <div class="welcome p-4">
+            <h3>Chào mừng đến với Lunar Archive</h3>
+            <p>Đây là một dự án lưu trữ dữ liệu cá nhân của bạn.</p>
+        </div>
+
+        <div class="about p-4">
+            <h4>Tại Lunar Archive, bạn có thể</h4><br>
+            <div class="d-flex flex-wrap justify-content-center">
+
+                <div class="card m-2" style="width: 300px">
+                    <img src="./images/data.png" width="300px" class="card-img-top border-bottom">
+                    <div class="card-body">
+                        <h5 class="card-title">Lưu trữ dữ liệu</h5>
+                        <p class="card-text">Lưu trữ dữ liệu tối ưu & an toàn</p>
+                    </div>
+                </div>
+
+                <div class="card m-2" style="width: 300px">
+                    <img src="./images/manage.png" width="300px" class="card-img-top border-bottom">
+                    <div class="card-body">
+                        <h5 class="card-title">Quản lý dữ liệu</h5>
+                        <p class="card-text">Quản lý dữ liệu của bạn ngay tại trang web</p>
+                    </div>
+                </div>
+
+                <div class="card m-2" style="width: 300px">
+                    <img src="./images/sharing.png" width="300px" class="card-img-top bỏder-bottom">
+                    <div class="card-body">
+                        <h5 class="card-title">Chia sẻ dữ liệu</h5>
+                        <p class="card-text">Chia sẻ dữ liệu của bạn với mọi người</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
