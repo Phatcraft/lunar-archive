bdc3981edb3273d054b3cab3eae1edb6c0c55680
Phatcraft <phathong0727@gmail.com>
2026-05-13 12:52:24 +0700

    scripts

---
+++ public/scripts/messages.js
@@ -0,0 +1,22 @@
+export function success_message(text){
+    var message = document.getElementById("message")
+    message.className = "bg-success p-2 text-white text-center"
+    message.innerHTML = text + ". Đang chuyển về trang chủ ."
+    
+    setTimeout(() => {
+        message.innerHTML = text + ". Đang chuyển về trang chủ . ."
+        setTimeout(() => {
+            message.innerHTML = text + ". Đang chuyển về trang chủ . . ."
+
+            setTimeout(() => {
+                location.replace("/")
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}
+
+export function failed_message(text){
+    var message = document.getElementById("message")
+    message.className = "bg-danger p-2 text-white text-center"
+    message.innerHTML = text
+}
